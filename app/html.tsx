7916c0b5da7a6164bd0e49ee17b1696dc5aa00cf
Createlier <axel1010bonnerot@gmail.com>
2026-09-01 09:34:33 +0200

    Activation de la PWA

---
+++ app/html.tsx
@@ -0,0 +1,43 @@
+import { ScrollViewStyleReset } from "expo-router/html";
+import type { PropsWithChildren } from "react";
+
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="fr">
+      <head>
+        <meta charSet="utf-8" />
+
+        <meta
+          httpEquiv="X-UA-Compatible"
+          content="IE=edge"
+        />
+
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <meta
+          name="theme-color"
+          content="#1976D2"
+        />
+
+        <link
+          rel="manifest"
+          href="/manifest.json"
+        />
+
+        <link
+          rel="apple-touch-icon"
+          href="/assets/images/apple-touch-icon.png"
+        />
+
+        <ScrollViewStyleReset />
+      </head>
+
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
